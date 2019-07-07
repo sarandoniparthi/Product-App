ef6cfb3b1f736563764f6fe681811c19cb440ff1
sarandoniparthi <saran.doniparthi@gmail.com>
2019-07-07 14:59:03 +0530

    new changes

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import './style.css';
+import {ProductDisplay} from './ProductDisplay';
 
-class App extends Component {
+export  class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      name: 'React'
-    };
+            products: [
+                { id: 1, name: "Kayak",
+                category: "Watersports", price: 275 },
+                { id: 2, name: "Lifejacket",
+                    category: "Watersports", price: 48.95 },
+                { id: 3, name: "Soccer Ball", category: "Soccer", price: 19.50 }
+            ]
+    }
+    
   }
 
   render() {
     return (
-      <div>       
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <div>
+      <ProductDisplay
+                    name="Products"
+                    products={ this.state.products }
+                    saveCallback={ p => this.saveData("products", p) }
+                    deleteCallback={ p => this.deleteData("products", p) } />
+     
+</div>
+
     );
   }
 }
 
 
-export default App;
