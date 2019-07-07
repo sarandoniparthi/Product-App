43e69699fc47bd2e5b02bbc75ea605e48105cd42
sarandoniparthi <saran.doniparthi@gmail.com>
2019-07-07 13:16:34 +0530

    initial setup

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 render(<App />, document.getElementById('root'));
