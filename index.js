a7b6afe98cf6d12d3fe9262dd8db8a88c7815269
sarandoniparthi <saran.doniparthi@gmail.com>
2019-07-07 21:15:26 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import {App} from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
